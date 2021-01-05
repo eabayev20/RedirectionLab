06a4ac4ca4b9753b1119edfd569473f8834249c8
eabayev20 <eabayev20@gmail.com>
2021-01-05 00:12:09 -0500

    Second PigLatin Done

--- PigLatin.java
+++ PigLatin.java
@@ -19,7 +19,17 @@ public class PigLatin{
   }
   public static String pigLatin(String s){
     s = s.toLowerCase();
-
+    String[] d;
+    String sss = "";
+    String[] d = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    int count = 0;
+    int al = d.length;
+    for (int i = 0; i != al; i++) {
+      if (s.substring(0,2).equals(d[i])) {
+        sss = s.substring(2,s.length()) + s.substring(0,2) + "ay";
+        return sss;
+      }
+    }
 
   }
 }
